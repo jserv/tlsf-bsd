ca0f893f81e8f9a9e8cdd552b3fccefaaae0a030
Jim Huang <jserv.tw@gmail.com>
2016-06-12 06:31:54 +0800

    clarify the copyright notice

--- tlsf/tlsf.c
+++ tlsf/tlsf.c
@@ -1,5 +1,7 @@
-/* COpyright (c) 2016 National Cheng Kung University. All rights reserved.
- * Copyright (c) 2006-2008, 2011, 2014 Matthew Conte. All rights reserved.
+/* Copyright (c) 2016 National Cheng Kung University, Taiwan.
+ * Copyright (c) 2006-2008, 2011, 2014 Matthew Conte.
+ * All rights reserved.
+ *
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
